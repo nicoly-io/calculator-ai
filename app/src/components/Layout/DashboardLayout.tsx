e674367aac9034c80014d53d7beab495dba421c6
tanjiro <toumanicoly53@gmail.com>
2026-01-22 12:30:52 +0000

    Add DashboardLayout component with sidebar support

--- app/src/components/Layout/DashboardLayout.tsx
+++ app/src/components/Layout/DashboardLayout.tsx
@@ -1,1 +1,13 @@
+import React from 'react';
+import { AppSidebar } from '../Navigation/AppSidebar';
+import { useMode } from '../../context/ModeContext';
 
+export const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  const { isSidebarOpen, setSidebarOpen, activeMode, setActiveMode } = useMode();
+  return (
+    <div className="min-h-screen bg-[#121212] text-white">
+      <AppSidebar activeMode={activeMode} setMode={setActiveMode} isOpen={isSidebarOpen} toggle={() => setSidebarOpen(!isSidebarOpen)} />
+      <main className={`transition-all duration-300 ${isSidebarOpen ? 'ml-64' : 'ml-0'}`}>{children}</main>
+    </div>
+  );
+};
