0725eca5420b3426a43f0503732d891c9f17d539
tanjiro <toumanicoly53@gmail.com>
2026-01-22 12:44:49 +0000

    Implement service worker registration function
    Add a function to register the service worker.

--- app/src/serviceWorkerRegistration.ts
+++ app/src/serviceWorkerRegistration.ts
@@ -1,1 +1,9 @@
-
+export function register() {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/sw.js').catch(err => {
+        console.error("Erreur SW:", err);
+      });
+    });
+  }
+}
