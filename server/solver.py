29d796b1ee45bd9bb2b75b1243a839b46b8fb2d2
tanjiro <toumanicoly53@gmail.com>
2026-01-22 12:42:55 +0000

    Add equation solver using Sympy

--- server/solver.py
+++ server/solver.py
@@ -1,1 +1,14 @@
+import sys
+import sympy
 
+def solve_equation(equation):
+    try:
+        # Nettoyage et résolution via Sympy
+        result = sympy.simplify(equation)
+        return result
+    except:
+        return "Equation non résoluble"
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        print(solve_equation(sys.argv[1]))
